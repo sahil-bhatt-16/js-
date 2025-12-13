25896ddeddf1d57b7459c3d4e9db7de8f4ac03de
sahil-bhatt-16 <dmssbhatt@gmail.com>
2025-12-13 15:05:43 +0000

    converstion of datatype

---
+++ .devcontainer/01_basics/02_dataType.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all JS code as =newer version 
+
+// alert(3 + 3) // we are using nodejs, not browser
+
+
+let name = "sahil"
+let age = 18
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint
+// stirng => ""
+// boolean => true/false
+// null => standalone vlaue 
+// undefined => 
+// symbol => unique
+
+// object =>
+
+
+    console.log(typeof "sahil");
